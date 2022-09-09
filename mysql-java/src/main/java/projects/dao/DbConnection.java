efcbaef58803dd792996a0ab3c6d56306a150226
JSchroff008 <jakeschroff@gmail.com>
2022-09-08 19:35:41 -0500

    Week-3 MySQL Promineo Tech

---
+++ mysql-java/src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,30 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import projects.exception.DbException;
+
+public class DbConnection {
+	private static String HOST = "localhost";
+	private static String PASSWORD = "project";
+	private static int PORT = 3306;
+	private static String SCHEMA = "project";
+	private static String USER = "project";
+
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, 
+				SCHEMA, USER, PASSWORD);
+		System.out.println("Connecting with user = " + url);
+
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Successfully obatained connection!");
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("Error getting connection.");
+			throw new DbException(e);
+		}
+	}
+
+}
